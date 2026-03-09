be4a15d1c78a6b2f860d07105f02ccbd3e28a68e
syncdecodes <syncdecodes@gmail.com>
2026-03-09 15:46:29 +0530

    modified app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const connectDB = async () => {
         const response = await mongoose.connect(process.env.MONGO_URL)
         console.log('connected to database')
 
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 5000, () => {
             console.log(`server is listening or port ${process.env.PORT}`)
         })
     }
